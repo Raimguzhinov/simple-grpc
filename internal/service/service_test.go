2e96b4305500bf30ca96f2b08fb472c82156979e
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2024-02-29 07:29:50 +0700

    Full GetEvents test cover

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -82,7 +82,7 @@ func TestMakeEvent(t *testing.T) {
 			Name:     testCase.actual.Name,
 		}
 		resp, err := s.MakeEvent(context.Background(), req)
-		t.Logf("\nCalling TestMakeEvent(\n  %v\n),\tresult: %d\n", testCase.actual, resp.GetEventId())
+		t.Logf("\nCalling TestMakeEvent(\n  %v\n),\nresult: %v\n\n", testCase.actual, resp.GetEventId())
 
 		// Assert:
 		if err != nil {
@@ -93,6 +93,7 @@ func TestMakeEvent(t *testing.T) {
 }
 
 func mockEventMaker(s *service.Server, currentTime int64) error {
+	oneMonthLater := time.UnixMilli(currentTime).AddDate(0, 1, 0).UnixMilli()
 	eventsData := []struct {
 		SenderId int64
 		Time     int64
@@ -100,6 +101,7 @@ func mockEventMaker(s *service.Server, currentTime int64) error {
 	}{
 		{SenderId: 1, Time: currentTime, Name: "User1"},
 		{SenderId: 2, Time: currentTime, Name: "User2"},
+		{SenderId: 2, Time: oneMonthLater, Name: "User2Again"},
 		{SenderId: 3, Time: currentTime, Name: "User3"},
 	}
 
@@ -116,11 +118,10 @@ func mockEventMaker(s *service.Server, currentTime int64) error {
 	return nil
 }
 
-func setupTest(t *testing.T) *service.Server {
+func setupTest(t *testing.T, currentTime time.Time) *service.Server {
 	s := service.RunEventsService()
-	currentTime := time.Now().UnixMilli()
 	t.Run("Mocking events", func(t *testing.T) {
-		err := mockEventMaker(s, currentTime)
+		err := mockEventMaker(s, currentTime.UnixMilli())
 		if err != nil {
 			t.Error(err)
 		}
@@ -131,8 +132,9 @@ func setupTest(t *testing.T) *service.Server {
 
 func TestGetEvent(t *testing.T) {
 	// Arrange:
-	s := setupTest(t)
-	currentTime := time.Now().UnixMilli()
+	currentTimeT := time.Now()
+	s := setupTest(t, currentTimeT)
+	currentTime := currentTimeT.UnixMilli()
 	loc, _ := time.LoadLocation("America/New_York")
 
 	testTable := []struct {
@@ -220,7 +222,7 @@ func TestGetEvent(t *testing.T) {
 			EventId:  testCase.actual.ID,
 		}
 		resp, err := s.GetEvent(context.Background(), req)
-		t.Logf("\nCalling TestGetEvent(\n  %v\n),\tresult: %d\n", testCase.actual, resp.GetEventId())
+		t.Logf("\nCalling TestGetEvent(\n  %v\n),\nresult: %v\n\n", testCase.actual, resp.GetEventId())
 
 		// Assert:
 		if err != nil {
@@ -235,7 +237,8 @@ func TestGetEvent(t *testing.T) {
 
 func TestDeleteEvent(t *testing.T) {
 	// Arrange:
-	s := setupTest(t)
+	currentTimeT := time.Now()
+	s := setupTest(t, currentTimeT)
 
 	testTable := []struct {
 		actual   models.Events
@@ -295,7 +298,7 @@ func TestDeleteEvent(t *testing.T) {
 			EventId:  testCase.actual.ID,
 		}
 		resp, err := s.DeleteEvent(context.Background(), req)
-		t.Logf("\nCalling TestDeleteEvent(\n  %v\n),\tresult: %d\n", testCase.actual, resp.GetEventId())
+		t.Logf("\nCalling TestDeleteEvent(\n  %v\n),\nresult: %v\n\n", testCase.actual, resp.GetEventId())
 
 		// Assert:
 		if err != nil {
@@ -307,23 +310,28 @@ func TestDeleteEvent(t *testing.T) {
 
 func TestGetEvents(t *testing.T) {
 	// Arrange:
-	s := setupTest(t)
 	currentTime := time.Now()
+	s := setupTest(t, currentTime)
 	oneYearAgo := currentTime.AddDate(-1, 0, 0).UnixMilli()
 	oneYearLater := currentTime.AddDate(1, 0, 0).UnixMilli()
 
-	mockStream := &mockEventStream{
-		sentEvents: make([]*eventmanager.Event, 0),
-	}
-
 	testTable := []struct {
-		actual   models.Events
-		expected []models.Events
+		actual    models.Events
+		timerange [2]int64
+		expected  []models.Events
 	}{
+		{
+			actual: models.Events{
+				SenderID: 0,
+			},
+			timerange: [2]int64{oneYearAgo, oneYearLater},
+			expected:  []models.Events{},
+		},
 		{
 			actual: models.Events{
 				SenderID: 1,
 			},
+			timerange: [2]int64{oneYearAgo, oneYearLater},
 			expected: []models.Events{
 				{
 					SenderID: 1,
@@ -333,31 +341,79 @@ func TestGetEvents(t *testing.T) {
 				},
 			},
 		},
+		{
+			actual: models.Events{
+				SenderID: 2,
+			},
+			timerange: [2]int64{oneYearAgo, oneYearLater},
+			expected: []models.Events{
+				{
+					SenderID: 2,
+					ID:       1,
+					Time:     currentTime.UnixMilli(),
+					Name:     "User2",
+				},
+				{
+					SenderID: 2,
+					ID:       2,
+					Time:     currentTime.AddDate(0, 1, 0).UnixMilli(),
+					Name:     "User2Again",
+				},
+			},
+		},
+		{
+			actual: models.Events{
+				SenderID: 3,
+			},
+			timerange: [2]int64{oneYearAgo, oneYearLater},
+			expected: []models.Events{
+				{
+					SenderID: 3,
+					ID:       1,
+					Time:     currentTime.UnixMilli(),
+					Name:     "User3",
+				},
+			},
+		},
+		{
+			actual: models.Events{
+				SenderID: 3,
+			},
+			timerange: [2]int64{oneYearAgo, oneYearAgo},
+			expected:  []models.Events{},
+		},
+		{
+			actual: models.Events{
+				SenderID: 3,
+			},
+			timerange: [2]int64{oneYearLater, oneYearLater},
+			expected:  []models.Events{},
+		},
 	}
 	// Act:
-	for i, testCase := range testTable {
+	for _, testCase := range testTable {
 		req := &eventmanager.GetEventsRequest{
 			SenderId: testCase.actual.SenderID,
-			FromTime: oneYearAgo,
-			ToTime:   oneYearLater,
+			FromTime: testCase.timerange[0],
+			ToTime:   testCase.timerange[1],
+		}
+		mockStream := &mockEventStream{
+			sentEvents: make([]*eventmanager.Event, 0),
 		}
 		err := s.GetEvents(req, mockStream)
-		// Assert:
-		t.Logf("\nCalling TestGetEvents(\n  %v\n),\tresult:\n", testCase.actual)
-		for _, sentEvent := range mockStream.sentEvents {
-			t.Log(sentEvent)
-		}
 		if err != nil {
-			t.Log(err)
+			t.Logf("\nCalling TestGetEvents(\n  %v\n),\nresult: %v\n\n", testCase.actual, err)
 			assert.Equal(t, err, service.ErrEventNotFound)
+			continue
 		}
-		if len(mockStream.sentEvents) != i+1 {
-			t.Fatalf("Expected %d event to be sent, got %d", i+1, len(mockStream.sentEvents))
+		t.Log(mockStream.sentEvents)
+		for i, resp := range mockStream.sentEvents {
+			t.Logf("\nCalling TestGetEvents(\n  %v\n),\nresult: %v\n\n", testCase.actual, resp)
+			assert.Equal(t, service.AccumulateEvent(testCase.expected[i]).GetSenderId(), resp.GetSenderId())
+			assert.Equal(t, service.AccumulateEvent(testCase.expected[i]).GetEventId(), resp.GetEventId())
+			assert.Equal(t, service.AccumulateEvent(testCase.expected[i]).GetName(), resp.GetName())
+			assert.Equal(t, service.AccumulateEvent(testCase.expected[i]).GetTime(), resp.GetTime())
 		}
-		assert.Equal(t, mockStream.sentEvents[i].GetSenderId(), service.AccumulateEvent(testCase.expected[i]).GetSenderId())
-		assert.Equal(t, mockStream.sentEvents[i].GetEventId(), service.AccumulateEvent(testCase.expected[i]).GetEventId())
-		assert.Equal(t, mockStream.sentEvents[i].GetName(), service.AccumulateEvent(testCase.expected[i]).GetName())
-		assert.Equal(t, mockStream.sentEvents[i].GetTime(), service.AccumulateEvent(testCase.expected[i]).GetTime())
 	}
 }
 
